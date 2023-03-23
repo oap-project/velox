fdb9dca596022b8ba7dbd604fee1256584959a16
ccat3z <lingfeng.zhang@intel.com>
2023-03-23 05:39:28 +0000

    Update centos7 setup script

--- scripts/setup-centos7.sh
+++ scripts/setup-centos7.sh
@@ -18,11 +18,13 @@ set -efx -o pipefail
 # so that some low level types are the same size. Also, disable warnings.
 SCRIPTDIR=$(dirname "${BASH_SOURCE[0]}")
 source $SCRIPTDIR/setup-helper-functions.sh
+DEPENDENCY_DIR=${DEPENDENCY_DIR:-/tmp/velox-deps}
 CPU_TARGET="${CPU_TARGET:-avx}"
 NPROC=$(getconf _NPROCESSORS_ONLN)
 export CFLAGS=$(get_cxx_flags $CPU_TARGET)  # Used by LZO.
 export CXXFLAGS=$CFLAGS  # Used by boost.
 export CPPFLAGS=$CFLAGS  # Used by LZO.
+export PKG_CONFIG_PATH=/usr/local/lib64/pkgconfig:/usr/local/lib/pkgconfig:/usr/lib64/pkgconfig:/usr/lib/pkgconfig:$PKG_CONFIG_PATH
 FB_OS_VERSION=v2022.11.14.00
 
 function run_and_time {
@@ -52,15 +54,17 @@ function wget_and_untar {
 }
 
 function install_cmake {
+  cd "${DEPENDENCY_DIR}"
   wget_and_untar https://cmake.org/files/v3.25/cmake-3.25.1.tar.gz cmake-3
   cd cmake-3
   ./bootstrap --prefix=/usr/local
-  make -j${nproc}
+  make -j$(nproc)
   make install
   cmake --version
 }
 
 function install_ninja {
+  cd "${DEPENDENCY_DIR}"
   github_checkout ninja-build/ninja v1.11.1
   ./configure.py --bootstrap
   cmake -Bbuild-cmake
@@ -69,16 +73,19 @@ function install_ninja {
 }
 
 function install_fmt {
+  cd "${DEPENDENCY_DIR}"
   github_checkout fmtlib/fmt 8.0.0
   cmake_install -DFMT_TEST=OFF
 }
 
 function install_folly {
+  cd "${DEPENDENCY_DIR}"
   github_checkout facebook/folly "${FB_OS_VERSION}"
   cmake_install -DBUILD_TESTS=OFF
 }
 
 function install_conda {
+  cd "${DEPENDENCY_DIR}"
   mkdir -p conda && cd conda
   wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
   MINICONDA_PATH=/opt/miniconda-for-velox
@@ -86,35 +93,38 @@ function install_conda {
 }
 
 function install_openssl {
+  cd "${DEPENDENCY_DIR}"
   wget_and_untar https://github.com/openssl/openssl/archive/refs/tags/OpenSSL_1_1_1s.tar.gz openssl
   cd openssl
-  ./config
+  ./config no-shared
   make depend
   make
   make install
-  mv /usr/bin/openssl /usr/bin/openssl.bak
-  ln -s /usr/local/ssl/bin/openssl /usr/bin/openssl  
 }
 
 function install_gflags {
+  cd "${DEPENDENCY_DIR}"
   wget_and_untar https://github.com/gflags/gflags/archive/v2.2.2.tar.gz gflags
   cd gflags
   cmake_install -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=ON -DBUILD_gflags_LIB=ON -DLIB_SUFFIX=64 -DCMAKE_INSTALL_PREFIX:PATH=/usr/local
 }
 
 function install_glog {
+  cd "${DEPENDENCY_DIR}"
   wget_and_untar https://github.com/google/glog/archive/v0.5.0.tar.gz glog
   cd glog
-  cmake_install -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX:PATH=/usr/local  
+  cmake_install -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=ON -DCMAKE_INSTALL_PREFIX:PATH=/usr/local  
 }
 
 function install_snappy {
+  cd "${DEPENDENCY_DIR}"
   wget_and_untar https://github.com/google/snappy/archive/1.1.8.tar.gz snappy
   cd snappy
   cmake_install -DSNAPPY_BUILD_TESTS=OFF  
 }
 
 function install_dwarf {
+  cd "${DEPENDENCY_DIR}"
   wget_and_untar https://github.com/davea42/libdwarf-code/archive/refs/tags/20210528.tar.gz dwarf
   cd dwarf
   #local URL=https://github.com/davea42/libdwarf-code/releases/download/v0.5.0/libdwarf-0.5.0.tar.xz
@@ -123,13 +133,50 @@ function install_dwarf {
   #wget -q --max-redirect 3 "${URL}"
   #tar -xf libdwarf-0.5.0.tar.xz -C "${DIR}"
   #cd dwarf/libdwarf-0.5.0
-  ./configure --enable-shared=yes
+  ./configure --enable-shared=no
   make
   make check
   make install
 }
 
+function install_re2 {
+  cd "${DEPENDENCY_DIR}"
+  wget_and_untar https://github.com/google/re2/archive/refs/tags/2023-03-01.tar.gz re2
+  cd re2
+  make install
+}
+
+function install_flex {
+  cd "${DEPENDENCY_DIR}"
+  wget_and_untar https://github.com/westes/flex/releases/download/v2.6.4/flex-2.6.4.tar.gz flex
+  cd flex
+  ./autogen.sh
+  ./configure
+  make install
+}
+
+function install_lzo {
+  cd "${DEPENDENCY_DIR}"
+  wget_and_untar http://www.oberhumer.com/opensource/lzo/download/lzo-2.10.tar.gz lzo
+  cd lzo
+  ./configure --prefix=/usr/local --enable-shared --disable-static --docdir=/usr/local/share/doc/lzo-2.10
+  make "-j$(nproc)"
+  make install
+}
+
+function install_boost {
+  cd "${DEPENDENCY_DIR}"
+  wget_and_untar https://boostorg.jfrog.io/artifactory/main/release/1.72.0/source/boost_1_72_0.tar.gz boost
+  cd boost
+  ./bootstrap.sh --prefix=/usr/local --with-python=/usr/bin/python3 --with-python-root=/usr/lib/python3.6 --without-libraries=python
+  ./b2 "-j$(nproc)" -d0 install threading=multi
+}
+
 function install_prerequisites {
+  run_and_time install_lzo
+  run_and_time install_boost
+  run_and_time install_re2
+  run_and_time install_flex
   run_and_time install_openssl
   run_and_time install_gflags
   run_and_time install_glog
@@ -143,26 +190,23 @@ function install_velox_deps {
   run_and_time install_conda
 }
 
-# CMAKE-3 install
-run_and_time install_cmake
-
-# ninja-build install
-run_and_time install_ninja
-
 # dnf install dependency libraries
 dnf_install epel-release dnf-plugins-core # For ccache, ninja
 # PowerTools only works on CentOS8
 # dnf config-manager --set-enabled powertools
 dnf_install ccache git wget which libevent-devel \
-  openssl-devel re2-devel libzstd-devel lz4-devel double-conversion-devel \
-  curl-devel cmake
+  openssl-devel libzstd-devel lz4-devel double-conversion-devel \
+  curl-devel cmake sudo
 
 dnf remove -y gflags
 
 # Required for Thrift
-dnf_install autoconf automake libtool bison flex python3
+dnf_install autoconf automake libtool bison python3 python3-devel
 
-dnf_install conda
+# Required for build flex
+dnf_install gettext-devel texinfo help2man
+
+# dnf_install conda
 
 # Activate gcc9; enable errors on unset variables afterwards.
 # GCC9 install via yum and devtoolset
@@ -173,36 +217,13 @@ source /opt/rh/devtoolset-9/enable || exit 1
 gcc --version
 set -u
 
-# Build from Sources
-# Fetch sources.
-wget_and_untar http://www.oberhumer.com/opensource/lzo/download/lzo-2.10.tar.gz lzo &
-wget_and_untar https://boostorg.jfrog.io/artifactory/main/release/1.72.0/source/boost_1_72_0.tar.gz boost &
-#wget_and_untar https://github.com/fmtlib/fmt/archive/8.0.0.tar.gz fmt &
+# Build from source
+[ -d "$DEPENDENCY_DIR" ] || mkdir -p "$DEPENDENCY_DIR"
 
-wait  # For cmake and source downloads to complete.
+run_and_time install_cmake
+run_and_time install_ninja
 
-# Build & install.
-# install lzo
-(
-  cd lzo
-  ./configure --prefix=/usr/local --enable-shared --disable-static --docdir=/usr/local/share/doc/lzo-2.10
-  make "-j$(nproc)"
-  make install
-)
-
-# install boost
-(
-  cd boost
-  ./bootstrap.sh --prefix=/usr/local --with-python=/usr/bin/python3 --with-python-root=/usr/lib/python3.6
-  ./b2 "-j$(nproc)" -d0 install threading=multi
-)
-
-# install prerequisites
 install_prerequisites
-
-# install fmt
-#cmake_install fmt -DFMT_TEST=OFF
-
 install_velox_deps
 
 dnf clean all
