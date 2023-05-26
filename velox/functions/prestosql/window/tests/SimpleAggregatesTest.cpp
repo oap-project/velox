56d8e71611153fbd518247cf144fa589904e59be
PHILO-HE <feilong.he@intel.com>
2023-05-26 17:34:14 +0800

    Add more test cases

--- velox/functions/prestosql/window/tests/SimpleAggregatesTest.cpp
+++ velox/functions/prestosql/window/tests/SimpleAggregatesTest.cpp
@@ -129,7 +129,7 @@ TEST_F(StringAggregatesTest, nonFixedWidthAggregate) {
 
 class KPreceedingFollowingTest : public WindowTestBase {};
 
-TEST_F(KPreceedingFollowingTest, rangeFrames) {
+TEST_F(KPreceedingFollowingTest, rangeFrames1) {
     auto vectors = makeRowVector({
       makeFlatVector<int64_t>({1, 1, 2147483650, 3, 2, 2147483650}),
       makeFlatVector<std::string>({"1", "1", "1", "2", "1", "2"}),
@@ -147,21 +147,37 @@ TEST_F(KPreceedingFollowingTest, rangeFrames) {
   testWindowFunction({vectors}, "count(c0)", {overClause}, kRangeFrames2);
 }
 
-TEST_F(KPreceedingFollowingTest, rangeFramesFailure) {
-    auto vectors = makeRowVector({
-      makeFlatVector<int64_t>({5, 5, 4, 6, 3, 2}),
-      makeFlatVector<std::string>({"1", "2", "2", "2", "1", "2"}),
-  });
+TEST_F(KPreceedingFollowingTest, rangeFrames2) {
+    const std::vector<RowVectorPtr> vectors = {
+      makeRowVector({
+      makeFlatVector<int64_t>({5, 6, 8, 9, 10, 2, 8, 9, 3}),
+      makeFlatVector<std::string>({"1", "1", "1", "1", "1", "2", "2", "2", "2"})}),
+      // Has repeated sort key.
+      makeRowVector({
+      makeFlatVector<int64_t>({5, 5, 3, 2, 8}),
+      makeFlatVector<std::string>({"1", "1", "1", "2", "1"})}),
+      makeRowVector({
+      makeFlatVector<int64_t>({5, 5, 4, 6, 3, 2, 8, 9, 9}),
+      makeFlatVector<std::string>({"1", "1", "2", "2", "1", "2", "1", "1", "2"})}),
+      };
 
   const std::string overClause = "partition by c1 order by c0";
   const std::vector<std::string> kRangeFrames = {
       "range between unbounded preceding and 1 following",
+      "range between unbounded preceding and 2 following",
+      "range between unbounded preceding and 3 following",
+      "range between 1 preceding and unbounded following",
+      "range between 2 preceding and unbounded following",
+      "range between 3 preceding and unbounded following",
+      "range between 1 preceding and 3 following",
+      "range between 3 preceding and 1 following",
+      "range between 2 preceding and 2 following"
   };
-  // workable.
-  // const std::vector<std::string> kRangeFrames = {
-  //     "range between current row and unbounded following",
-  // };
-  testWindowFunction({vectors}, "avg(c0)", {overClause}, kRangeFrames);
+  for (int i = 0; i < vectors.size(); i++) {
+    testWindowFunction({vectors[i]}, "avg(c0)", {overClause}, kRangeFrames);
+    testWindowFunction({vectors[i]}, "sum(c0)", {overClause}, kRangeFrames);
+    testWindowFunction({vectors[i]}, "count(c0)", {overClause}, kRangeFrames);
+  }
 }
 
 TEST_F(KPreceedingFollowingTest, rowsFrames) {
