8a48dc12a8f68ffbc9ff5203fdb1040a0e1e4b5b
rui-mo <rui.mo@intel.com>
2023-08-17 16:00:12 +0800

    support decimal compare function

--- velox/functions/sparksql/DecimalVectorFunctions.cpp
+++ velox/functions/sparksql/DecimalVectorFunctions.cpp
@@ -14,11 +14,12 @@
  * limitations under the License.
  */
 
+#include "velox/functions/sparksql/DecimalVectorFunctions.h"
 #include "velox/expression/DecodedArgs.h"
 #include "velox/expression/VectorFunction.h"
 #include "velox/functions/prestosql/ArithmeticImpl.h"
+#include "velox/functions/sparksql/DecimalUtil.h"
 #include "velox/type/DecimalUtil.h"
-#include "velox/functions/sparksql/DecimalVectorFunctions.h"
 
 namespace facebook::velox::functions::sparksql {
 namespace {
@@ -40,7 +41,7 @@ class MakeDecimalFunction final : public exec::VectorFunction {
     auto unscaledVec = decodedArgs.at(0);
     auto result = resultRef->asUnchecked<FlatVector<T>>()->mutableRawValues();
     if constexpr (std::is_same_v<T, int64_t>) {
-      int128_t bound = DecimalUtil::kPowersOfTen[precision_];
+      int128_t bound = velox::DecimalUtil::kPowersOfTen[precision_];
       rows.applyToSelected([&](int row) {
         auto unscaled = unscaledVec->valueAt<int64_t>(row);
         if (unscaled <= -bound || unscaled >= bound) {
@@ -235,19 +236,326 @@ class DecimalRoundFunction : public exec::VectorFunction {
       uint8_t rScale,
       int32_t scale) const {
     if (scale >= 0) {
-      auto rescaledValue = DecimalUtil::rescaleWithRoundUp<A, R>(
+      auto rescaledValue = velox::DecimalUtil::rescaleWithRoundUp<A, R>(
           a, aPrecision, aScale, rPrecision, rScale);
       VELOX_DCHECK(rescaledValue.has_value());
       r = rescaledValue.value();
     } else {
-      auto reScaleFactor = DecimalUtil::kPowersOfTen[aScale - scale];
-      DecimalUtil::divideWithRoundUp<R, A, int128_t>(
+      auto reScaleFactor = velox::DecimalUtil::kPowersOfTen[aScale - scale];
+      velox::DecimalUtil::divideWithRoundUp<R, A, int128_t>(
           r, a, reScaleFactor, false, 0, 0);
-      r = r * DecimalUtil::kPowersOfTen[-scale];
+      r = r * velox::DecimalUtil::kPowersOfTen[-scale];
     }
   }
 };
+
+// Rescale two inputs as the same scale and compare. Returns 0 when a is equal
+// with b. Returns -1 when a is less than b. Returns 1 when a is greater than b.
+template <typename T>
+int32_t rescaleAndCompare(T a, T b, int32_t deltaScale) {
+  T aScaled = a;
+  T bScaled = b;
+  if (deltaScale < 0) {
+    aScaled = a * velox::DecimalUtil::kPowersOfTen[-deltaScale];
+  } else if (deltaScale > 0) {
+    bScaled = b * velox::DecimalUtil::kPowersOfTen[deltaScale];
+  }
+  if (aScaled == bScaled) {
+    return 0;
+  } else if (aScaled < bScaled) {
+    return -1;
+  } else {
+    return 1;
+  }
+}
+
+// Compare two decimals. Rescale one of them When they are of different scales.
+int32_t decimalCompare(
+    int128_t a,
+    uint8_t aPrecision,
+    uint8_t aScale,
+    int128_t b,
+    uint8_t bPrecision,
+    uint8_t bScale) {
+  int32_t deltaScale = aScale - bScale;
+  // Check if we need 256-bits after adjusting the scale.
+  bool need256 = (deltaScale < 0 &&
+                  aPrecision - deltaScale > LongDecimalType::kMaxPrecision) ||
+      (bPrecision + deltaScale > LongDecimalType::kMaxPrecision);
+  if (need256) {
+    return rescaleAndCompare<int256_t>(
+        static_cast<int256_t>(a), static_cast<int256_t>(b), deltaScale);
+  }
+  return rescaleAndCompare<int128_t>(a, b, deltaScale);
+}
+
+// GreaterThan decimal compare function.
+class Gt {
+ public:
+  inline static bool apply(
+      int128_t a,
+      uint8_t aPrecision,
+      uint8_t aScale,
+      int128_t b,
+      uint8_t bPrecision,
+      uint8_t bScale) {
+    return decimalCompare(a, aPrecision, aScale, b, bPrecision, bScale) > 0;
+  }
+};
+
+// GreaterThanOrEqual decimal compare function.
+class Gte {
+ public:
+  inline static bool apply(
+      int128_t a,
+      uint8_t aPrecision,
+      uint8_t aScale,
+      int128_t b,
+      uint8_t bPrecision,
+      uint8_t bScale) {
+    return decimalCompare(a, aPrecision, aScale, b, bPrecision, bScale) >= 0;
+  }
+};
+
+// LessThan decimal compare function.
+class Lt {
+ public:
+  inline static bool apply(
+      int128_t a,
+      uint8_t aPrecision,
+      uint8_t aScale,
+      int128_t b,
+      uint8_t bPrecision,
+      uint8_t bScale) {
+    return decimalCompare(a, aPrecision, aScale, b, bPrecision, bScale) < 0;
+  }
+};
+
+// LessThanOrEqual decimal compare function.
+class Lte {
+ public:
+  inline static bool apply(
+      int128_t a,
+      uint8_t aPrecision,
+      uint8_t aScale,
+      int128_t b,
+      uint8_t bPrecision,
+      uint8_t bScale) {
+    return decimalCompare(a, aPrecision, aScale, b, bPrecision, bScale) <= 0;
+  }
+};
+
+// Equal decimal compare function.
+class Eq {
+ public:
+  inline static bool apply(
+      int128_t a,
+      uint8_t aPrecision,
+      uint8_t aScale,
+      int128_t b,
+      uint8_t bPrecision,
+      uint8_t bScale) {
+    return decimalCompare(a, aPrecision, aScale, b, bPrecision, bScale) == 0;
+  }
+};
+
+// Class for decimal compare operations.
+template <
+    typename A /* Argument */,
+    typename B /* Argument2 */,
+    typename Operation /* Arithmetic operation */>
+class DecimalCompareFunction : public exec::VectorFunction {
+ public:
+  DecimalCompareFunction(
+      uint8_t aPrecision,
+      uint8_t aScale,
+      uint8_t bPrecision,
+      uint8_t bScale)
+      : aPrecision_(aPrecision),
+        aScale_(aScale),
+        bPrecision_(bPrecision),
+        bScale_(bScale) {}
+
+  void apply(
+      const SelectivityVector& rows,
+      std::vector<VectorPtr>& args,
+      const TypePtr& resultType,
+      exec::EvalCtx& context,
+      VectorPtr& result) const override {
+    prepareResults(rows, resultType, context, result);
+
+    // Fast path when the first argument is a flat vector.
+    if (args[0]->isFlatEncoding()) {
+      auto rawA = args[0]->asUnchecked<FlatVector<A>>()->mutableRawValues();
+
+      if (args[1]->isConstantEncoding()) {
+        auto constantB = args[1]->asUnchecked<SimpleVector<B>>()->valueAt(0);
+        context.applyToSelectedNoThrow(rows, [&](auto row) {
+          result->asUnchecked<FlatVector<bool>>()->set(
+              row,
+              Operation::apply(
+                  (int128_t)rawA[row],
+                  aPrecision_,
+                  aScale_,
+                  (int128_t)constantB,
+                  bPrecision_,
+                  bScale_));
+        });
+        return;
+      }
+
+      if (args[1]->isFlatEncoding()) {
+        auto rawB = args[1]->asUnchecked<FlatVector<B>>()->mutableRawValues();
+        context.applyToSelectedNoThrow(rows, [&](auto row) {
+          result->asUnchecked<FlatVector<bool>>()->set(
+              row,
+              Operation::apply(
+                  (int128_t)rawA[row],
+                  aPrecision_,
+                  aScale_,
+                  (int128_t)rawB[row],
+                  bPrecision_,
+                  bScale_));
+        });
+        return;
+      }
+    } else {
+      // Fast path when the first argument is encoded but the second is
+      // constant.
+      exec::DecodedArgs decodedArgs(rows, args, context);
+      auto aDecoded = decodedArgs.at(0);
+      auto aDecodedData = aDecoded->data<A>();
+
+      if (args[1]->isConstantEncoding()) {
+        auto constantB = args[1]->asUnchecked<SimpleVector<B>>()->valueAt(0);
+        context.applyToSelectedNoThrow(rows, [&](auto row) {
+          auto value = aDecodedData[aDecoded->index(row)];
+          result->asUnchecked<FlatVector<bool>>()->set(
+              row,
+              Operation::apply(
+                  (int128_t)value,
+                  aPrecision_,
+                  aScale_,
+                  (int128_t)constantB,
+                  bPrecision_,
+                  bScale_));
+        });
+        return;
+      }
+    }
+
+    // Decode the input in all other cases.
+    exec::DecodedArgs decodedArgs(rows, args, context);
+    auto aDecoded = decodedArgs.at(0);
+    auto bDecoded = decodedArgs.at(1);
+
+    auto aDecodedData = aDecoded->data<A>();
+    auto bDecodedData = bDecoded->data<B>();
+
+    context.applyToSelectedNoThrow(rows, [&](auto row) {
+      auto aValue = aDecodedData[aDecoded->index(row)];
+      auto bValue = bDecodedData[bDecoded->index(row)];
+      result->asUnchecked<FlatVector<bool>>()->set(
+          row,
+          Operation::apply(
+              (int128_t)aValue,
+              aPrecision_,
+              aScale_,
+              (int128_t)bValue,
+              bPrecision_,
+              bScale_));
+    });
+  }
+
+ private:
+  void prepareResults(
+      const SelectivityVector& rows,
+      const TypePtr& resultType,
+      exec::EvalCtx& context,
+      VectorPtr& result) const {
+    context.ensureWritable(rows, resultType, result);
+    result->clearNulls(rows);
+  }
+
+  const uint8_t aPrecision_;
+  const uint8_t aScale_;
+  const uint8_t bPrecision_;
+  const uint8_t bScale_;
+};
+
+template <typename Operation>
+std::shared_ptr<exec::VectorFunction> createDecimalCompareFunction(
+    const std::string& name,
+    const std::vector<exec::VectorFunctionArg>& inputArgs,
+    const core::QueryConfig& /*config*/) {
+  auto aType = inputArgs[0].type;
+  auto bType = inputArgs[1].type;
+  auto [aPrecision, aScale] = getDecimalPrecisionScale(*aType);
+  auto [bPrecision, bScale] = getDecimalPrecisionScale(*bType);
+  if (aType->isShortDecimal()) {
+    if (bType->isShortDecimal()) {
+      return std::make_shared<
+          DecimalCompareFunction<int64_t, int64_t, Operation>>(
+          aPrecision, aScale, bPrecision, bScale);
+    } else if (bType->isLongDecimal()) {
+      return std::make_shared<
+          DecimalCompareFunction<int64_t, int128_t, Operation>>(
+          aPrecision, aScale, bPrecision, bScale);
+    }
+  }
+  if (aType->isLongDecimal()) {
+    if (bType->isShortDecimal()) {
+      return std::make_shared<
+          DecimalCompareFunction<int128_t, int64_t, Operation>>(
+          aPrecision, aScale, bPrecision, bScale);
+    } else if (bType->isLongDecimal()) {
+      return std::make_shared<
+          DecimalCompareFunction<int128_t, int128_t, Operation>>(
+          aPrecision, aScale, bPrecision, bScale);
+    }
+  }
+  VELOX_UNREACHABLE();
+}
+
+std::vector<std::shared_ptr<exec::FunctionSignature>>
+decimalCompareSignature() {
+  return {exec::FunctionSignatureBuilder()
+              .integerVariable("a_precision")
+              .integerVariable("a_scale")
+              .integerVariable("b_precision")
+              .integerVariable("b_scale")
+              .returnType("boolean")
+              .argumentType("DECIMAL(a_precision, a_scale)")
+              .argumentType("DECIMAL(b_precision, b_scale)")
+              .build()};
+}
+
 } // namespace
+
+VELOX_DECLARE_STATEFUL_VECTOR_FUNCTION(
+    udf_decimal_gt,
+    decimalCompareSignature(),
+    createDecimalCompareFunction<Gt>);
+
+VELOX_DECLARE_STATEFUL_VECTOR_FUNCTION(
+    udf_decimal_gte,
+    decimalCompareSignature(),
+    createDecimalCompareFunction<Gte>);
+
+VELOX_DECLARE_STATEFUL_VECTOR_FUNCTION(
+    udf_decimal_lt,
+    decimalCompareSignature(),
+    createDecimalCompareFunction<Lt>);
+
+VELOX_DECLARE_STATEFUL_VECTOR_FUNCTION(
+    udf_decimal_lte,
+    decimalCompareSignature(),
+    createDecimalCompareFunction<Lte>);
+
+VELOX_DECLARE_STATEFUL_VECTOR_FUNCTION(
+    udf_decimal_eq,
+    decimalCompareSignature(),
+    createDecimalCompareFunction<Eq>);
 
 std::vector<std::shared_ptr<exec::FunctionSignature>>
 makeDecimalByUnscaledValueSignatures() {
