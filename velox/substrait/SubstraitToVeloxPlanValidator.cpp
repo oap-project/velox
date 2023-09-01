d148e00545d7fc93c4160dfffd0bf8327086ce6b
PHILO-HE <feilong.he@intel.com>
2023-09-01 11:26:28 +0800

    Revert an unnecessary change

--- velox/substrait/SubstraitToVeloxPlanValidator.cpp
+++ velox/substrait/SubstraitToVeloxPlanValidator.cpp
@@ -174,7 +174,7 @@ bool SubstraitToVeloxPlanValidator::validateScalarFunction(
       return false;
     }
   }
-  if (name == "murmur3hash" || name == "xxhash64") {
+  if (name == "murmur3hash") {
     for (const auto& type : types) {
       if (type.find("struct") != std::string::npos ||
           type.find("map") != std::string::npos ||
